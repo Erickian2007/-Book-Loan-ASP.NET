4d0c2b514688cdb9113f10e1273ab6afd175daad
Erickian2007 <erickian2007@gmail.com>
2023-08-12 13:05:17 -0300

    model-created

--- Models/EmprestimosModel.cs
+++ Models/EmprestimosModel.cs
@@ -1,6 +1,13 @@
-﻿namespace EmprestimoLivros.Models
+﻿using Microsoft.VisualBasic;
+
+namespace EmprestimoLivros.Models
 {
     public class EmprestimosModel
     {
+        public int Id { get; set; }
+        public string? Recebedor { get; set; }
+        public string? Fornecedor { get; set; }
+        public string? LivroEmprestado { get; set; }
+        public DateAndTime? DataEmprestimo { get; set; }
     }
 }
