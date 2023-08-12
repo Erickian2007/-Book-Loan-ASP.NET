10dd8c775cecff8d65b6a8dde8f3e1064b48eac2
Erickian2007 <erickian2007@gmail.com>
2023-08-12 15:13:28 -0300

    part-1-completed

---
+++ Controllers/EmprestimosController.cs
@@ -0,0 +1,20 @@
+﻿using EmprestimoLivros.Data;
+using EmprestimoLivros.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmprestimoLivros.Controllers
+{
+    public class EmprestimosController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public EmprestimosController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            IEnumerable<EmprestimosModel> emprestimos = _db.Emprestimos;
+            return View(emprestimos);
+        }
+    }
+}
